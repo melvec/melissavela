c6ea1ede4d6d19fee0ba4f1abd50c6af96c6363c
melissa <melissa.vela.coral@gmail.com>
2020-07-04 19:27:37 +1000

    first commit Melissa Vela

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get("/", (req,res)=>{
+    res.render('index.html', {title:'Melissa Vela'});
+});
+
+router.get("/contact", (req,res)=>{
+    res.render('contact.html', {title:'Melissa Vela'});
+});
+
+ 
+module.exports = router;
